4fe5ffbedd6268b411843482a6bb640d0d69e3cd
mtrosz <mltroszczynski@gmail.com>
2018-01-28 17:55:03 +0100

    User.cs added.

---
+++ FlightBookingSystemClassLibrary/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBookingSystemClassLibrary
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surename { get; set; }
+        public string Country { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsAdult { get; set; }
+        public string EmailAdress { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
