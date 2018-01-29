62e39d2dbdc533295878970a13ef46d501c2a9e3
robadam <35897668+robadam@users.noreply.github.com>
2018-01-29 21:02:22 +0100

    Klasy Airport i Airplane
    Proste klasy Airport i Airplane

---
+++ FlightBookingSystemClassLibrary/Airplane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBookingSystemClassLibrary
+{
+    class Airplane
+    {
+        public string Producer { get; set; }
+        public string Model { get; set; }
+        public int MaxCapacity { get; set; }
+        public float CruiseSpeed { get; set; }
+
+    }
+}
---
+++ FlightBookingSystemClassLibrary/Airport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBookingSystemClassLibrary
+{
+    class Airport
+    {
+        public string ICAO { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Continent { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+
+}
